43fa69a5e25adc0afb8f47156a6c18734210e351
Joao Pontel <moluscojp4@gmail.com>
2025-10-16 20:12:25 -0300

    Atualizando css para entrega

---
+++ script.js
@@ -0,0 +1,22 @@
+  const select = document.getElementById('nomes');
+    
+    // dispara quando a opção muda
+    select.addEventListener('change', () => {
+      esconderImagens()  
+      const value = select.options[select.selectedIndex];  
+      const id = value.getAttribute("data-alvo");
+      const img = document.getElementById(id);
+      if (img.style.display === "none") {
+        img.style.display = "block"
+      }
+      
+      
+    });
+
+
+    function esconderImagens() {
+      const imagens = document.querySelectorAll(".imagens__tabela"); // pega todas as imagens
+      imagens.forEach(img => {
+        img.style.display = "none"; // aplica display: none
+      });
+    }
